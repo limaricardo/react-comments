7dbb250d18b6448127fe0afd0bbb1255646f5d40
Ricardo <limaricardop@gmail.com>
2022-02-04 12:28:35 -0300

    Multiple Props

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import CommentDetail from './CommentDetail';
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author="Sam" />
-            <CommentDetail author="Jane" />
-            <CommentDetail author="Alex" />
-            <CommentDetail author="Mary" />
+            <CommentDetail author="Sam" timeAgo="Today at 4:40PM" commentText="Hello man" profileImage={faker.image.image()}/>
+            <CommentDetail author="Jane" timeAgo="Today at 4:50PM" commentText="Nice blog post" profileImage={faker.image.image()} />
+            <CommentDetail author="Alex" timeAgo="Today at 5:40PM" commentText="What is all about?" profileImage={faker.image.image()} />
+            <CommentDetail author="Mary" timeAgo="Today at 5:50PM" commentText="Test that one" profileImage={faker.image.image()} />
         </div>
 
     );
